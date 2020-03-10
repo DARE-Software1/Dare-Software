c90793b3916d7d946a5f83797158b52adab61308
Danielson9712 <55254935+Danielson9712@users.noreply.github.com>
2020-03-09 21:54:43 -0400

    nav bar v. 1

--- DARE/view/nav.php
+++ DARE/view/nav.php
@@ -1,24 +1,13 @@
 <nav>
-    <ul>
+    
 
 
-        <li>
-            <a href='index.php?action=studentLogin'> Student</a>
-        </li>
-
-        <li>
-            <a href='index.php?action=staffLogin'> Staff</a>
-        </li>
-        <li   
-            <a    style="float:right;
-                padding-right: 0px;
-                font-size: 19px";
-
-                href='index.php?action=stuLogoutBut'> logout</a>
-
-
-        </li>
-    </ul>
-
+        
+            <a style = "width: 33.33%;" href='index.php?action=studentLogin'> Student</a>
+        
+            <a style = "width: 33.33%;" href='index.php?action=staffLogin'> Staff</a>
+         
+            <a style = "width: 33.33%;" href ='index.php?action=stuLogoutBut'> Logout </a>
+                
 
 </nav>
